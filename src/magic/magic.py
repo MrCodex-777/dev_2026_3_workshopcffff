44760255d55a21cde5c57c07d74ee7144ed640ce
Gabriel Betancourt <gmbetancourt23@ucatolica.edu.co>
2026-08-24 17:49:47 -0500

    ahora si, todo correcto en magic y yo que me alegro

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -15,7 +15,7 @@ class Magic:
             int: El n-ésimo número de Fibonacci
         """
         if n < 0:
-            raise ValueError("n debe ser un entero no negativo")
+            return None
         elif n == 0:
             return 0
         elif n == 1:
@@ -130,7 +130,7 @@ class Magic:
             int: El factorial de n
         """
         if n < 0:
-            raise ValueError("n debe ser un entero no negativo")
+            return None
         if n == 0:
             return 1
         else:
