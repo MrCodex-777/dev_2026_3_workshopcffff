d6edfae4b629e370031ba0d572275bcf0255582b
Gabriel Betancourt <gmbetancourt23@ucatolica.edu.co>
2026-08-25 19:25:05 -0500

    están un poco raras esas pruebas... bueno, magic solo una mal, que lástima...

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -131,10 +131,15 @@ class Magic:
         """
         if n < 0:
             return None
-        if n == 0:
+        elif n == 0:
+            return 0
+        elif n == 1:
             return 1
         else:
-            return n * self.factorial(n - 1)
+            a, b = 0, 1
+            for _ in range(2, n + 1):
+                a, b = b, a + b
+            return b
 
     def mcd(self, a, b):
         """
